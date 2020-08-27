083f81ef3ddbf1445f67a966592e19fdda1cc2f6
thingley <trevorhingley@hotmail.com>
2020-08-27 18:29:03 +0100

    Initial XUnit test added.

---
+++ Database/TPH.Chariot.Database.Test/TestBase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+
+using TPH.Chariot.Data.Legacy.DataPortal;
+using TPH.Chariot.Data.Legacy.Common.Interfaces;
+
+namespace TPH.Chariot.Database.Test
+{
+	public class TestBase
+	{
+		protected IDataPortal DataPortal { get; private set; }
+
+		public TestBase()
+		{
+			var config = new ConfigurationBuilder().AddJsonFile("TPH.Chariot.Database.Test.config.json").Build();
+			string connectionString = config["ConnectionString"];
+
+			DataPortal = new DataPortal();
+			DataPortal.ConnectionString = connectionString;
+		}
+	}
+}
---
+++ Database/TPH.Chariot.Database.Test/Test__customer__Customer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using System.Data;
+using Xunit;
+
+using TPH.Chariot.Data.Legacy.Common.DataTableFactory;
+using TPH.Chariot.Data.Legacy.Common.Interfaces;
+using TPH.Chariot.Data.Legacy.DataPortal;
+
+namespace TPH.Chariot.Database.Test
+{
+	public class Test__customer__Customer: TestBase
+	{
+		[Fact]
+		public void CreateValidCustomers()
+		{
+			DataTableFactory dataTableFactory = new DataTableFactory();
+			DataTable customers = dataTableFactory.customer__Customer();
+
+			customers.Rows.Add(customers.NewRow().ItemArray = new object[] { 0, 0, "AAA", "AAA Finance", 1});
+
+			IDataPortalResult result = DataPortal.Persist__customer__Customer(customers);
+
+			Assert.True(result.OK, "Database operation returned error.");
+			Assert.Equal(1, result.RowsUpdated);
+		}
+	}
+}
